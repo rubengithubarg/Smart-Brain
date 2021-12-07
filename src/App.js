c9c244961347cf2fd16cff3ceda47e9c8c661939
Ruben <emaranda94@gmail.com>
2021-12-07 11:55:03 -0300

    Smart Brian Frontend v3

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Particles from 'react-particles-js';
 import Navigation from './components/Navigation/Navigation';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
@@ -39,31 +40,42 @@ class App extends Component {
   }
 
   onInputChange = (event) => {
-    console.log(event.target.value);
+    this.setState({input: event.target.value});
   }
 
   onButtonSubmit = () => {
-    console.log('click');
+    this.setState({imageUrl: this.state.input})
+    //console.log('click');
+    App.models
+     .predict(
+       Clarifai.FACE_DETECTION_MODEL,
+       this.state.input)
+      .then(
+        function(response) {
+          console.log(response.outputs[0].data.regions[0].region_info.bounding_box)
+        },
+        function(err) {
+          //There was an error.
+        }
+      );
   }
 
   render() {
 
     return (
 
-      
       <div className="App">
           <Particles className='particles'
-            params={particlesOptions}
+              params={particlesOptions}
+            />
+          <Navigation />
+          <Logo />
+          <Rank />
+          <ImageLinkForm 
+            onInputChange={this.onInputChange}
+            onButtonSubmit={this.onButtonSubmit}
           />
-        <Navigation />
-        <Logo />
-        <Rank />
-        <ImageLinkForm 
-          onInputChange={this.onInputChange}
-          onButtonSubmit={this.onButtonSubmit}
-        />
-          {/*
-            <FaceRecognition /> */}
+          <FaceRecognition imageUrl={this.state.imageUrl}/>
       </div>
     );
   }
